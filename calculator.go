a18bff9b564b1d0b36e4a3be6f47a12c79eb3569
Luís Pinto <luispinto23@gmail.com>
2022-05-15 01:02:28 +0100

    refactor: refactored test case and README file

--- calculator.go
+++ calculator.go
@@ -1,6 +1,34 @@
 package calculator
 
-// Add adds two floating numbers together
+import "errors"
+
+// Divide returns the quotient of a and b.
+func Divide(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, errors.New("division by zero not allowed")
+	}
+	return a / b, nil
+}
+
+// Add returns the sum of two numbers.
 func Add(a, b float64) float64 {
 	return a + b
+}
+
+// Subtract returns the difference of two numbers.
+func Subtract(a, b float64) float64 {
+	return a - b
+}
+
+// Multiply returns the product of two numbers
+func Multiply(a, b float64) float64 {
+	return a * b
+}
+
+// Sqrt returns the square root of a number or error if the number is negative.
+func Sqrt(a float64) (float64, error) {
+	if a < 0 {
+		return 0, errors.New("square root of negative number not allowed")
+	}
+	return a * a, nil
 }
--- calculator_test.go
+++ calculator_test.go
@@ -2,15 +2,31 @@ package calculator_test
 
 import (
 	"calculator"
+	"errors"
 	"testing"
 )
 
-func TestAdd(t *testing.T) {
-	var want float64 = 15
+func TestDivide(t *testing.T) {
+	t.Parallel()
+	testCases := []struct {
+		a, b float64
+		want float64
+		err  error
+	}{
+		{a: 10, b: 5, want: 2, err: nil},
+		{a: 120, b: 0, want: 0.0, err: errors.New("division by zero not allowed")},
+		{a: 99, b: 0, want: 0.0, err: errors.New("division by zero not allowed")},
+		{a: 9, b: 3, want: 3, err: nil},
+	}
 
-	got := calculator.Add(5, 10)
+	for _, tc := range testCases {
+		got, err := calculator.Divide(tc.a, tc.b)
 
-	if got != want {
-		t.Errorf("got %f want %f", got, want)
+		if err != nil {
+			t.Fatalf("Divide(%f, %f): want no error for valid input, got %v", tc.a, tc.b, err)
+		}
+		if tc.want != got {
+			t.Errorf("Divide(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
 	}
 }
